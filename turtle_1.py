1cf9ad4d39dc3d86507db45958c06b90babdf075
Manjeet Kumar <mkj22834@gmail.com>
2025-11-04 18:43:02 +0530

    turtle_1.py

---
+++ turtle_1.py
@@ -0,0 +1,27 @@
+# command to check turtle module is installed on the pc python -m turtle
+
+import turtle
+# how to draw a square
+# turtle.getscreen()
+# turtle.forward(100)
+# turtle.left(90) # left rotation by 90 degree
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+
+# how to change shape
+# turtle.shape("turtle")
+# turtle.shape("circle")
+# turtle.shape("square")
+# turtle.shape("arrow")
+# turtle.shape("classic")
+
+# how to change color
+# turtle.color("red")
+
+turtle.forward(100)
+turtle.rt(45) # right rotation by 45 degree
+turtle.forward(100)
+turtle.exitonclick()
