be214971653b0d803428f07097dcbbe98b4b4957
Manjeet Kumar <mkj22834@gmail.com>
2025-11-09 09:58:36 +0530

    operator_overloading.py modified

--- operator_overloading.py
+++ operator_overloading.py
@@ -1,3 +1,4 @@
+# Example 1
 class ComplexNumber:
     def __init__(self, r, i):
         self.real = r
@@ -13,3 +14,22 @@ c2 = ComplexNumber(4,5)
 c3 = c1 + c2
 print(c3.real,c3.imaginary)
 
+
+# Example 2
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def __gt__(self, other):
+        if self.age > other.age:
+            return True
+        else:
+            return False
+
+p1 = Person("John", 90)
+p2 = Person("John", 60)
+
+if p1 > p2:
+    print("p1 is greater than p2")
+else:
+    print("p2 is greater than p1")
