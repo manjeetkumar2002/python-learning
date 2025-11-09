7fb1bfc16b4243c2920076dfa1e4fdfbccff85bc
Manjeet Kumar <mkj22834@gmail.com>
2025-11-09 17:12:23 +0530

    exercise27 is modified

--- exercise27.py
+++ exercise27.py
@@ -2,7 +2,6 @@ class BankAccount:
     def __init__(self,name,balance):
         self.name = name
         self.balance = balance
-
     def withdraw(self,amount):
         if amount > self.balance:
             print("Insufficient balance")
@@ -10,7 +9,6 @@ class BankAccount:
         self.balance -= amount
         print("Amount withdrawn successfully")
         print("current balance is",self.balance)
-
     def deposit(self,amount):
         if amount < 0:
             print("Invalid amount")
@@ -20,18 +18,20 @@ class BankAccount:
     def show(self):
         print("Your name is : ",self.name)
         print("Your balance is :",self.balance)
-
     def change_name(self,new_name):
         self.name = new_name
-
     def get_balance(self):
         return self.balance
-
     def get_name(self):
         return self.name
+    def __str__(self):
+        return f"Your name is {self.name}\nYour balance is {self.balance}"
+
 acc1 = BankAccount("John",100)
 acc1.deposit(100)
 acc1.withdraw(50)
-acc1.show()
+print(acc1)
 acc1.change_name("jenny")
-acc1.show()
+print(acc1)
+acc1.withdraw(1000)
+print(acc1)
