6ce76d24b4fe953653c0b4417e8664e68451bd1c
Manjeet Kumar <mkj22834@gmail.com>
2025-11-11 17:35:25 +0530

    added brute force solution in middle of linked list

--- middle_of_sll.py
+++ middle_of_sll.py
@@ -6,7 +6,9 @@ list.append(20)
 list.append(30)
 list.append(40)
 list.append(50)
+list.append(60)
 
+#opitimal approach
 def middle_element(head):
     if head is None:
         return -1
@@ -17,4 +19,16 @@ def middle_element(head):
         fast = fast.next.next
     return slow.val
 
-print("middle element is :",middle_element(list.head))
+# Brute force
+def middle_element1(head):
+    curr = head
+    count = 0
+    while curr is not None:
+        count += 1
+        curr = curr.next
+
+    curr = head
+    for i in range(0,count//2):
+        curr = curr.next
+    return curr.val
+print("middle element is :",middle_element1(list.head))
