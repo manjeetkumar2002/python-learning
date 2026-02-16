162278b31a8bffac0b571f475058236dd5bde624
Manjeet Kumar <mkj22834@gmail.com>
2026-02-16 20:35:43 +0530

    max consecutive ones 3 optimal and better solution added

--- maxConsecutiveOnes3.py
+++ maxConsecutiveOnes3.py
@@ -14,3 +14,48 @@ class Solution:
                 maxi = max(maxi, j - i + 1)
 
         return maxi
+
+
+#better
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        maxi = 0
+        left = 0
+        right = 0
+        zeros = 0
+        while right < len(nums):
+            if nums[right] == 0:
+                zeros += 1
+
+            if zeros > k:
+                while zeros != k:
+                    if nums[left] == 0:
+                        zeros -= 1
+                    left += 1
+
+            maxi = max(maxi, right - left + 1)
+            right += 1
+
+        return maxi
+
+
+#optimal
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        maxi = 0
+        left = 0
+        right = 0
+        zeros = 0
+        while right < len(nums):
+            if nums[right] == 0:
+                zeros += 1
+
+            if zeros > k:
+                if nums[left] == 0:
+                    zeros -= 1
+                left += 1
+            if zeros <= k:
+                maxi = max(maxi, right - left + 1)
+            right += 1
+
+        return maxi
