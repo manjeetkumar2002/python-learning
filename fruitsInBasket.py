f299b2e27242838b054934c82d4d92680c216622
Manjeet Kumar <mkj22834@gmail.com>
2026-02-17 11:01:32 +0530

    fruits in basket optimal added

--- fruitsInBasket.py
+++ fruitsInBasket.py
@@ -31,5 +31,26 @@ class Solution:
 
             if len(my_dict) <= 2:
                 maxi = max(maxi , right-left+1)
-                right+=1
+            right+=1
+        return maxi
+
+
+class Solution:
+    def totalFruit(self, nums: List[int]) -> int:
+        maxi = 0
+        n = len(nums)
+        left = 0
+        right = 0
+        my_dict = dict()
+        while right<n:
+            my_dict[nums[right]] = my_dict.get(nums[right], 0) + 1
+            if len(my_dict) > 2:
+                my_dict[nums[left]]-=1
+                if my_dict[nums[left]] == 0:
+                    del my_dict[nums[left]]
+                left+=1
+
+            if len(my_dict) <= 2:
+                maxi = max(maxi , right-left+1)
+            right+=1
         return maxi
